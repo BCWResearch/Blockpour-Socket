e28d62d0ba8bde00f94ee2ea28104d83473b6e34
sandeep-bcw <sandeep@bcw.group>
2022-03-04 04:19:21 +0530

    feat: socket added

--- index.js
+++ index.js
@@ -10,52 +10,47 @@ const options = {
   rejectUnauthorized: process.env.REJECT_UNAUTHORIZED_CERT === 'true'
 }
 
-const sslServer = https.createServer(options, app);
 
 
 const { Server } = require("socket.io");
-const io = new Server();
 const sslPort = process.env.SOCKET_SERVER_SSL_PORT
 let emitSocket;
 const clientIO = require('socket.io-client');
 const RedisUtilClass = require('./utils/redisUtils');
 const clientSocket = clientIO.connect(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`, { reconnect: true,  "rejectUnauthorized": false });
+
+const sslServer = https.createServer(options, app);
+
+const io = new Server();
+
 clientSocket.on('connect', async function () {
   console.log('Connected to the indexer socket! ');
 
-  let channelsCreated = false
 
   const topTokens = await RedisUtilClass.getKeys(`toptoken_*`)
-  while(!channelsCreated){
     if (topTokens.data) {
   
       for (let data of topTokens.data) {
         const tokenSymbol = await RedisUtilClass.getValue(data)
-        const tS = tokenSymbol.data
+        const tS = tokenSymbol.data.replace(/"/g, ''); 
         if (tS) {
           clientSocket.on(tS, function (from, msg) {
-            console.log(` ${tS} received message from ${from} as  ===> ${msg}`)
             if (emitSocket) {
               emitSocket.emit(tS, from, msg);
             }
           })
           clientSocket.on('AllCrypto', function (from, msg) {
-            console.log(` ${from} received message ===> ${msg}`)
             if (emitSocket) {
               emitSocket.emit('AllCrypto',from, msg );
             }
           })          
         }
       }
-      channelsCreated = true
-    }
   }
 });
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 io.on('connection', (socket) => {
+  console.log("received the connection")
   emitSocket = socket
 });
 
