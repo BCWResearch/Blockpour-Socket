f99d2c8c759aebd101f839de90754db8e578ce12
sandeep-bcw <sandeep@bcw.group>
2022-04-01 01:19:04 +0530

    feat: multichannel socket fix

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ const options = {
 const { Server } = require("socket.io");
 const sslPort = process.env.SOCKET_SERVER_SSL_PORT
 const allCryptoSocket = process.env.ALL_CRYPTO_SOCKET
-let emitSocket;
+let emitSockets = [];
 const clientIO = require('socket.io-client');
 const RedisUtilClass = require('./utils/redisUtils');
 const clientSocket = clientIO.connect(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`, { reconnect: true,  "rejectUnauthorized": false });
-
 app.use(cors())
 
 const sslServer = https.createServer(options, app);
@@ -36,40 +35,29 @@ const socketCreated = {}
 clientSocket.on('connect', async function () {
   console.log('Connected to the indexer socket! ');
 
-
-  const topTokens = await RedisUtilClass.getKeys(`toptoken_*`)
-    if (topTokens.data) {
-      for (let data of topTokens.data) {
-        const tokenSymbol = await RedisUtilClass.getValue(data)
-        const tS = tokenSymbol.data.replace(/"/g, ''); 
-        if (tS && !socketCreated[tS]) {
-          socketCreated[tS] = true
-          clientSocket.on(tS, function (from, msg) {
-            if (emitSocket) {
-              emitSocket.emit(tS, from, msg);
-            }
-          })
-        }
-                
-        }
-    }
-  if(!socketCreated[allCryptoSocket]){
-    socketCreated[allCryptoSocket] = true
     clientSocket.on(allCryptoSocket, function (from, msg) {
-      if (emitSocket) {
-        emitSocket.emit(allCryptoSocket,from, msg );
+      const temp = JSON.parse(msg)
+
+      if (emitSockets.length) {
+        emitSockets.forEach(emitSocket => {
+          emitSocket.emit(allCryptoSocket,from, msg );
+        })
       }
     })  
-  }
+  
+  clientSocket.on("disconnect", (reason) => {
+    console.log(reason); // "ping timeout"
+  });
 });
 
+
 io.on('connection', (socket) => {
-  console.log("received the connection")
-  emitSocket = socket
+  console.log("Received the new connection")
+  emitSockets.push(socket)
 });
 
 sslServer.listen( sslPort, function() {
-  console.log(  `Listening HTTPS on ${sslPort}` );
+  console.log(`Listening HTTPS on ${sslPort}` );
 });
 
 io.attach(sslServer)
