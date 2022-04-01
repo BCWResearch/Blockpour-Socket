a890bccaeb6b2f3ce5a7cb47f51d68d0288cff7e
sandeep-bcw <sandeep@bcw.group>
2022-04-01 20:59:59 +0530

    feat: recursive socket added

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ const options = {
 const { Server } = require("socket.io");
 const sslPort = process.env.SOCKET_SERVER_SSL_PORT
 const allCryptoSocket = process.env.ALL_CRYPTO_SOCKET
-let emitSockets = [];
 const clientIO = require('socket.io-client');
 const RedisUtilClass = require('./utils/redisUtils');
-const clientSocket = clientIO.connect(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`, { reconnect: true,  "rejectUnauthorized": false });
 app.use(cors())
 
 const sslServer = https.createServer(options, app);
@@ -30,32 +28,37 @@ const io = new Server({
   }
 });
 
-
-clientSocket.on('connect', async function () {
-  console.log('Connected to the indexer socket! ');
-
-    clientSocket.on(allCryptoSocket, function (from, msg) {
-      if (emitSockets.length) {
-        emitSockets.forEach(emitSocket => {
-          emitSocket.emit(allCryptoSocket,from, msg );
-        })
-      }
-    })  
+const connectToTheClient = function () {
+  console.log("Connecting to the socket server")
+  let clientSocket = clientIO.connect(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`, { reconnect: true,  "rejectUnauthorized": false });
   
-  clientSocket.on("disconnect", (reason) => {
-    console.log(reason); // "ping timeout"
-    clientSocket.disconnect()
-    emitSockets.forEach(emitSocket => {
-      emitSocket.disconnect()
-    })
-    emitSockets = []
+  clientSocket.on('connect', async function () {
+    console.log('Connected to the indexer socket! ');
+  
+      clientSocket.on(allCryptoSocket, function (from, msg) {
+          console.log("received the data")
+            io.emit(allCryptoSocket,from, msg );
+      })  
+    
+      clientSocket.on("disconnect", (reason) => {
+        
+      });
+      clientSocket.on("connect_error", (reason) => {
+        clientSocket.disconnect()
+        console.log("connect_error called"); // "ping timeout"
+        connectToTheClient()
+    });
   });
-});
+}
+connectToTheClient()
 
 
 io.on('connection', (socket) => {
   console.log("Received the new connection")
-  emitSockets.push(socket)
+});
+
+io.on('disconnect', (socket) => {
+  console.log("Disconnected the new connection ", socket.conn.id)
 });
 
 sslServer.listen( sslPort, function() {
